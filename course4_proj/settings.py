3bee6d0d192b77c13db12f90bd9a7e1d87ba732d
Adriaan Henning <henningfah@gmail.com>
2025-03-25 03:13:59 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -20,7 +20,8 @@ class Dev(Configuration):
     BASE_DIR = Path(__file__).resolve().parent.parent
     #OMDB_KEY = values.SecretValue()
     OMDB_KEY = "cc5347f8"
-
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    ADMINS = [("Ben", "ben@example.com")]
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
